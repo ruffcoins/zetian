8b2328de23850ed95ce4f61a3a79b84d4b05946d
Anthonynzube <nzubeanthony4@gmail.com>
2021-04-30 15:54:08 +0100

    create service profile screen

---
+++ lib/screens/service/serviceprofile.dart
@@ -0,0 +1,255 @@
+import 'package:flutter/material.dart';
+import 'package:zetian/screens/service/updateservice.dart';
+
+class ServiceProfile extends StatefulWidget {
+  @override
+  _ServiceProfileState createState() => _ServiceProfileState();
+}
+
+class _ServiceProfileState extends State<ServiceProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white),
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
+              ),
+            )),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: PopupMenuButton(
+              elevation: 10,
+              itemBuilder: (BuildContext context) => [
+                PopupMenuItem(
+                  padding:
+                      EdgeInsets.only(top: 30, right: 30, left: 30, bottom: 30),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UpdateService()));
+                    },
+                    child: Text(
+                      "Update",
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                PopupMenuItem(
+                  padding: EdgeInsets.only(bottom: 30, right: 30, left: 30),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      "Delete",
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.red,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth >= 768) {
+            //
+            // Tablet View
+            //
+            return Column(
+              children: [
+                // Name Section
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: Icon(
+                    Icons.home_repair_service,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                ),
+                // Next Section
+                Expanded(
+                    child: Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 15),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(34))),
+                  child: ListView(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 50),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Service Name',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: 33, right: 25, left: 25, bottom: 25),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Amount',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 20,
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '5000',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 20,
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ))
+              ],
+            );
+          } else {
+            //
+            //Mobile View
+            //
+            return Column(
+              children: [
+                // Name Section
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: Icon(
+                    Icons.home_repair_service,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                ),
+                // Next Section
+                Expanded(
+                    child: Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 15),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(34))),
+                  child: ListView(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 50),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Service Name',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: 33, right: 25, left: 25, bottom: 25),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Amount',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 20,
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '5000',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 20,
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => UpdateService(),
+                              ));
+                        },
+                        child: Container(
+                          margin:
+                              EdgeInsets.only(top: 20, right: 20.0, left: 20.0),
+                          height: 40,
+                          // height: MediaQuery.of(context).size.height * 0.05,
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Colors.greenAccent,
+                            color: Colors.green,
+                            elevation: 7.0,
+                            // Register Button
+                            child: Center(
+                              child: Text(
+                                'UPDATE',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Montserrat',
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ))
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
