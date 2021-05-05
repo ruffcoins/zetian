42c446339dd0f61d033475a63a4235ff380b634f
Anthonynzube <nzubeanthony4@gmail.com>
2021-05-05 14:57:13 +0100

    Add dashboard route and sales route to side menu

--- lib/partials/sidemenu.dart
+++ lib/partials/sidemenu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zetian/screens/customer/viewcustomers.dart';
+import 'package:zetian/screens/dashboard.dart';
+import 'package:zetian/screens/sale/viewsales.dart';
 import 'package:zetian/screens/service/viewservices.dart';
 
 class SideMenu extends StatelessWidget {
@@ -37,6 +39,29 @@ class SideMenu extends StatelessWidget {
           decoration: BoxDecoration(
             color: Colors.green,
           ),
+        ),
+        ListTile(
+          title: Text(
+            'Dashboard',
+            style: TextStyle(
+                fontSize: 18.0,
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w500),
+          ),
+          onTap: () {
+            // Then close the drawer
+            Navigator.pop(context);
+
+            // Update the state of the app
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => Dashboard()),
+                (route) => false);
+          },
+        ),
+        Divider(
+          height: 2.0,
+          color: Colors.black45,
         ),
         ListTile(
           title: Text(
@@ -89,10 +114,12 @@ class SideMenu extends StatelessWidget {
                 fontWeight: FontWeight.w500),
           ),
           onTap: () {
-            // Update the state of the app
-            // ...
             // Then close the drawer
             Navigator.pop(context);
+
+            // Update the state of the app
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => ViewSales()));
           },
         ),
         Divider(
