1eb2e6a4016ac6d68198f3a3b61807671343d941
markjordan <thurggex@gmail.com>
2021-11-08 17:20:17 +0100

    Properly returned the relevant error message and returned 400 by default.

--- lib/data/authentication/login/login_data.dart
+++ lib/data/authentication/login/login_data.dart
@@ -3,29 +3,26 @@ import 'package:zetian/models/authentication/login/login_request.dart';
 import 'package:zetian/utils/operation.dart';
 
 class LoginData {
-  Response lresponse = Response(
-      statusCode: 400,
-      requestOptions: RequestOptions(
-          data: {"message": "Error occurred"}, path: "/users/login"));
-
   Future<Operation> loginUser(
       Dio dio, LoginRequest request, String baseUrl) async {
     try {
       Response response = await dio.post(
           "https://zeitan.herokuapp.com/users/login",
-          data: {"username": "ebuka", "password": "help12345"});
-      //request.toJson());
-      // {
-      //   "username": "adekunle3",
-      //   "password": "adekunlegold"
-      // });
+          data: request.toJson());
+      // {"username": "ebuka", "password": "help12345"});
+
       print("Got here");
       print(response.data);
+      print(response.statusCode);
       return Operation(response.statusCode, response.data);
     } on DioError catch (e) {
-      print("DioError ${e.message}");
+      try {
+        return Operation(e.response!.statusCode, e.message);
+      }
+      catch(e){
+        return Operation(400, "Error Occurred");
+      }
     }
-    return Operation(lresponse.statusCode, lresponse.data.toString());
   }
 }
 
