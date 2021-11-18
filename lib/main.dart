f2334b5046555c0aa809e8f27e62ec41a3e02e17
markjordan <thurggex@gmail.com>
2021-11-18 01:34:24 +0100

    Added named routes for navigation robustness.
    Added UserProvider

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,14 @@ import 'package:zetian/providers/employee_provider.dart';
 import 'package:zetian/providers/expense_provider.dart';
 import 'package:zetian/providers/sale_provider.dart';
 import 'package:zetian/providers/service_provider.dart';
+import 'package:zetian/providers/user_provider.dart';
+import 'package:zetian/screens/basic/customer/viewcustomers.dart';
+import 'package:zetian/screens/basic/dashboard.dart';
+import 'package:zetian/screens/basic/employee/viewemployees.dart';
+import 'package:zetian/screens/basic/expense/viewexpenses.dart';
+import 'package:zetian/screens/basic/sale/viewsales.dart';
+import 'package:zetian/screens/basic/service/viewservices.dart';
+import 'package:zetian/screens/basic/users/userlist.dart';
 import 'package:zetian/screens/login.dart';
 import 'package:zetian/screens/register.dart';
 
@@ -29,16 +37,28 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => SaleProvider()),
         ChangeNotifierProvider(create: (_) => DashboardProvider()),
         ChangeNotifierProvider(create: (_) => AuthenticationProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        home: Login(),
-        // routes: {
-        //   'dashboard'
-        // },
+        //home: Login(),
+        initialRoute: '/',
+        routes: {
+          // When navigating to the "/" route, build the FirstScreen widget.
+          '/': (context) => Login(),
+          // When navigating to the "/second" route, build the SecondScreen widget.
+          '/dashboard': (context) => Dashboard(),
+          '/register': (context) => Register(),
+          '/customers': (context) => ViewCustomers(),
+          '/services': (context) => ViewServices(),
+          '/sales': (context) => ViewSales(),
+          '/expenses': (context) => ViewExpense(),
+          '/users': (context) => ViewUsers(),
+          '/employees': (context) => ViewEmployees(),
+        },
       ),
     );
   }
