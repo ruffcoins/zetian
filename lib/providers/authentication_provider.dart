037bcedc2aacf9a290832fd822fac32aacd59abf
markjordan <thurggex@gmail.com>
2021-11-18 01:26:46 +0100

    Added userInformation response to the authentication to enable use of token.

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:zetian/models/authentication/login/login_response.dart';
 
 class AuthenticationProvider extends ChangeNotifier {
   bool isLoading = false;
+  LoginResponse? result;
 
   void updateIsLoading(bool isLoadingGotten) {
     isLoading = isLoadingGotten;
+    notifyListeners();
+  }
+
+  void updateUserInformation(LoginResponse userResult) {
+    result = userResult;
     notifyListeners();
   }
 }
