ada205ee7af2ee66a73ae50bfdb5601d3d3d17af
markjordan <thurggex@gmail.com>
2021-11-18 01:37:03 +0100

    Removed TabletView

--- lib/screens/basic/expense/expenselist.dart
+++ lib/screens/basic/expense/expenselist.dart
@@ -48,82 +48,8 @@ class _ExpenseListState extends State<ExpenseList> with ExpenseHelper {
               ),
             ),
           ),
-          endDrawer: SideMenu(),
           body: LayoutBuilder(
             builder: (context, constraints) {
-              if (constraints.maxWidth >= 768) {
-                //
-                // Tablet View
-                //
-                // If Expenses list is empty show the empty Expenses screen
-                if (expenseList == false) {
-                  return Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      // crossAxisAlignment: CrossAxisAlignment.center ,
-                      children: [
-                        Text(
-                          'No Expenses',
-                          style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 28.0,
-                          ),
-                        ),
-                        Icon(
-                          Icons.error,
-                          color: Colors.black38,
-                          size: 50.0,
-                        )
-                      ],
-                    ),
-                  );
-                } else {
-                  // If Expenses list is not empty, show Expenses list
-                  return Center(
-                    child: Container(
-                      padding: EdgeInsets.only(top: 20.0, bottom: 50.0),
-                      constraints: BoxConstraints(maxWidth: 600),
-                      child: ListView.builder(
-                        itemCount: 20,
-                        itemBuilder: (context, index) {
-                          return Card(
-                            //                           <-- Card widget
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.account_balance_wallet,
-                                size: 30,
-                              ),
-                              title: Text(
-                                'New Equipment',
-                                style: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              subtitle: Text(
-                                'Nov 7',
-                                style: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              trailing: Text(
-                                '₦10000',
-                                style: TextStyle(
-                                    color: Colors.red,
-                                    fontFamily: 'Montserrat',
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  );
-                }
-              } else {
-                //
                 // Mobile View
                 //
                 // If Expenses list is empty show the empty Expenses screen
@@ -221,7 +147,6 @@ class _ExpenseListState extends State<ExpenseList> with ExpenseHelper {
                           ),
                         );
                 }
-              }
             },
           ),
         );
