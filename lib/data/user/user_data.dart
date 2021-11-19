d41389db61e1e45368761c4b00352d5ffdec603b
markjordan <thurggex@gmail.com>
2021-11-20 00:24:43 +0100

    Removed unused imports and included a little print test (to be removed in later commit).

--- lib/data/user/user_data.dart
+++ lib/data/user/user_data.dart
@@ -1,7 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:zetian/models/employee/create/create_new_employee_request.dart';
-import 'package:zetian/models/employee/read/get_employee_response.dart';
-import 'package:zetian/models/employee/update/update_employee_request.dart';
 import 'package:zetian/models/user/read/get_user_response.dart';
 import 'package:zetian/utils/operation.dart';
 
@@ -10,19 +7,19 @@ class UserData {
   Future<Operation> getAllUsers(Dio dio, String baseUrl, String token) async {
     try {
       Response response =
-      await dio.get("https://zeitan.herokuapp.com/employees", options: Options(
+      await dio.get("https://zeitan.herokuapp.com/users", options: Options(
           headers: {
             "Authorization": "$token"
           }
       ));
-      UserResponse data = UserResponse.fromJson(response.data);
+      print("Response: $response");
+      var data = UserResponse.fromJson(response.data);
       print("message from user data");
       return Operation(response.statusCode, data);
     } on DioError catch (e) {
-      print(e.message);
+      print("Error ${e.message}");
+      return Operation(500, "Problems");
     }
-
-    return Operation(500, "Problems");
   }
 
 }
