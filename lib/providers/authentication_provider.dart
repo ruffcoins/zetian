e1224258858b9a60b08efa11186aa92609552f8c
markjordan <thurggex@gmail.com>
2021-11-19 23:30:38 +0100

    Created the setStartInfo function for user token persistence across sessions.

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -10,6 +10,13 @@ class AuthenticationProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  // This function is called to help the shared_preference package repopulate the LoginInformation of the user
+  // so as to enable other functionality that requires the token and id of user to operate without having to
+  // make an API call
+  void setStartInfo(String username, String token, String id, String role, int v){
+    result = LoginResponse(success: true, message: LoginMessage(id: id, username: username, role: role, v: v), token: token);
+  }
+
   void updateUserInformation(LoginResponse userResult) {
     result = userResult;
     notifyListeners();
